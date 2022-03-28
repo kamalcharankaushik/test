d120acab1aee270b6771594e92b7b7d8e50df8d2
Kamal Charan <charankamal459@gmail.com>
2022-03-28 13:28:52 +0530

    testing

--- src/main/java/restservices/helloworld/test/SampleTest.java
+++ src/main/java/restservices/helloworld/test/SampleTest.java
@@ -8,6 +8,12 @@ public class SampleTest {
 	public String method() {
 System.out.print("hello");
 		return "I am kamal charan kaushik(kck)";
+		
 
+	}
+
+	public String fun() {
+		// TODO Auto-generated method stub
+		return "Hello kamal charan";
 	}
 }
